785943b49cdf0b126d053bb334861103c5300dcd
TristanAK <kbrtri001@myuct.ac.za>
2023-09-03 20:51:55 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Typography, Box, useTheme } from "@mui/material";
 import {tokens} from "../theme";
-
+//changed
 const Header = ({title, subtitle}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
