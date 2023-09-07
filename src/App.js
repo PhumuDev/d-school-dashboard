6008c5c02947ab496ec4dbf742260612e828ae39
PhumuDev <140548721+PhumuDev@users.noreply.github.com>
2023-09-07 17:54:25 +0200

    Add Page Routes
    Added page routes allowing different sidebar items to link to different pages

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import TopBar from "./scenes/global/TopBar";
 import SideBar from './scenes/global/SideBar';
 import Overview from './scenes/overview';
 import { useState } from 'react';
-import Chart from "react-apexcharts";
 import './App.css';
-import Electricity from './scenes/electricity';
-import  Water  from './scenes/water';
+import SolarGeneration from './scenes/electricity/solar generation';
+import TotalWaterUsage from './scenes/water/total water usage';
+import EnergyConsumption from './scenes/electricity/energy consumption';
+import CostSavings from './scenes/electricity/cost savings';
 
 
 //import Water from './scenes/water';
@@ -34,8 +35,10 @@ function App() {
           <TopBar />
            <Routes>
             <Route path="/" element={<Overview/>}/>
-            <Route path="/electricity" element={<Electricity/>}/> 
-            <Route path="/water" element={<Water/>}/>
+            <Route path="/solar generation" element={<SolarGeneration/>}/> 
+            <Route path="/energy consumption" element={<EnergyConsumption/>}/>
+            <Route path="/cost savings" element={<CostSavings/>}/>  
+            <Route path="/total water usage" element={<TotalWaterUsage/>}/>
             </Routes>
             
           </main>
