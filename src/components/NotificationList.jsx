e5fc66f3bb0c974564ebf747c851af1518f1e177
TristanAK <kbrtri001@myuct.ac.za>
2023-09-23 16:07:08 +0200

    Update NotificationList
    NotificationList now adds notifications using an array. The array just needs to be fetched from the server.

--- src/components/NotificationList.jsx
+++ src/components/NotificationList.jsx
@@ -34,6 +34,16 @@ const NotificationList = () => {
     
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    const allNotifications = [
+        ["Solar Generation Optimal", "The solar panels are generating the optimal amount of energy.",colors.greenAccent[300]],
+        ["High Energy Usage", "Energy usage is currently 2x above the above the expected usage.",colors.redAccent[300]],
+        ["Low Water Consumption", "The building is consuming 2x less than the expected consumption.",colors.greenAccent[300]],
+    ];
+    const notis=[];
+    for (let i =0;i<allNotifications.length;i++) {
+        notis.push(<Notification title={allNotifications[i][0]} subtext={allNotifications[i][1]} nc={allNotifications[i][2]}/>);
+        
+    }
     return(
         <div className = "notificationList" style={{backgroundColor: colors.primary[400], border:"3px solid"+ colors.primary[500]}}>
                 <div className = "middle-container">
@@ -46,21 +56,7 @@ const NotificationList = () => {
                         Notifications
                     </Typography>
                 </div>
-                <Notification 
-                title="Solar Generation Optimal" 
-                subtext="The solar panels are generating the optimal amount of energy."
-                nc={colors.greenAccent[300]}
-                />
-                <Notification 
-                title="High Energy Usage" 
-                subtext="Energy usage is currently 2x above the above the expected usage."
-                nc={colors.redAccent[300]}
-                />
-                <Notification 
-                title="Low Water Consumption" 
-                subtext="The building is consuming 2x less than the expected consumption."
-                nc={colors.greenAccent[300]}
-                />
+                {notis}
 
     </div>
     )
